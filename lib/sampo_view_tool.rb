23f91df0f145a77a403cb8c07d24b23a8844243a
Sampo Kuokkanen <sampo.kuokkanen@gmail.com>
2019-06-14 21:39:19 +0900

    Build the gem file

--- lib/sampo_view_tool.rb
+++ lib/sampo_view_tool.rb
@@ -1,6 +1,4 @@
 require "sampo_view_tool/version"
 require "sampo_view_tool/renderer"
 module SampoViewTool
-  class Error < StandardError; end
-  # Your code goes here...
 end
--- lib/sampo_view_tool/renderer.rb
+++ lib/sampo_view_tool/renderer.rb
@@ -1,5 +1,7 @@
-class Renderer
-  def self.copyright(name, msg)
-    "&copy; #{Time.now.year} | <b>#{name}</b> #{msg}".html_safe
+module SampoViewTool 
+  class Renderer
+    def self.copyright(name, msg)
+      "&copy; #{Time.now.year} | <b>#{name}</b> #{msg}".html_safe
+    end
   end
 end
